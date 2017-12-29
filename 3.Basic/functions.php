b3f9261fd127c3fbbe9283abcdfea65c2a60995a
U-RHOST\lol <monster>
2017-12-29 14:05:16 +0800

    ppp

--- 3.Basic/functions.php
+++ 3.Basic/functions.php
@@ -1,3 +1,14 @@
 <?php
 
-print_r(get_defined_functions());
+$arr = file("E:/www/3.Basic/database.php");
+
+foreach ($arr as $item => $value) {
+    echo <<<BBB
+<div style="color: red">
+   <pre>第 $item 行:     $value</pre>
+</div>
+BBB;
+
+}
+
+// print_r(get_defined_functions());
