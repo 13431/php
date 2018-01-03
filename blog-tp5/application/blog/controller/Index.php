dc8b9bcb217d08d4422fa165e90582041a177f2f
U-RHOST\lol <monster>
2018-01-03 16:09:15 +0800

    ppp

--- blog-tp5/application/blog/controller/Index.php
+++ blog-tp5/application/blog/controller/Index.php
@@ -5,9 +5,29 @@ use think\Controller;
 
 class Index extends Controller
 {
-    public function index()
-    {
+    public function index() {
+        $this->assign("user", db("user")->where('username', '=', 'admin')->select());
+        $this->assign("cates", db("category")->select());
+        $this->assign("articles", db("article")->paginate(2));
+        return $this->fetch();
+    }
+
+    public function demo() {
         // 获取数据
+        $t = db("article");
+        $t->select();  // 所有数据
+        $t->count();   // 条目
+        $t->find();    // 取出第一条
+        $t->value('name');  // 取出某个值
+        $t->column('');     // 某一列信息
+        $t->limit(5)
+            ->order('id desc')
+            ->where('name' , '<>', 'xxx')
+            ->where('', '', '')
+            ->select();
+
+        $t->paginate(2);
+
         // 渲染给用户
 
         $this->assign("wannengdeshuzu", ['name'=>'鲁迅', 'book'=>'故事新编']);
@@ -19,6 +39,5 @@ class Index extends Controller
             ['name'=>'xxx之道', 'price'=>223],
             ]);
         return $this->fetch();
-
     }
 }
--- blog-tp5/application/config.php
+++ blog-tp5/application/config.php
@@ -236,6 +236,6 @@ return [
     'paginate'               => [
         'type'      => 'bootstrap',
         'var_page'  => 'page',
-        'list_rows' => 15,
+        'list_rows' => 3,
     ],
 ];
